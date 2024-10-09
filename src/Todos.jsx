0b115c78088a32b828207553c6c69adc9bb6f67b
Jeevikamathi <113614036+jeevikamathi@users.noreply.github.com>
2024-10-09 16:56:06 +0000

    Updated Context Provider Component

--- src/Todos.jsx
+++ src/Todos.jsx
@@ -1,54 +1,110 @@
+// import React, { useState, useContext, createContext } from "react";
+
+// const TodoContext = createContext();
+
+// const Myprovider = ({ children }) => {
+//   const [todo, setTodo] = useState("");
+//   const [todos, setTodos] = useState([]);
+
+//   const addTodo = () => {
+//     setTodos([...todos, todo]);
+//     setTodo("");
+//   };
+//   return (
+//     <TodoContext.Provider value={{ todos, setTodos, addTodo, todo, setTodo }}>
+//       {children}
+//     </TodoContext.Provider>
+//   );
+// };
+// const TodoForm = (props) => {
+//   const { todo, setTodo, addTodo } = useContext(TodoContext);
+//   return (
+//     <>
+//       {props.title}
+//       <input
+//         type="text"
+//         value={todo}
+//         placeholder="Add your todo"
+//         onChange={(e) => setTodo(e.target.value)}
+//       />
+//       <button type="submit" onClick={props.itemTodo}>
+//         Add Todo
+//       </button>
+//     </>
+//   );
+// };
+
+// function TodoList(props) {
+//   console.log(props);
+//   const { todos } = useContext(TodoContext);
+
+//   return (
+//     <ul>
+//       {todos.length > 0 &&
+//         todos.map((todo, index) => <TodoItem key={index} todo={todo} />)}
+//     </ul>
+//   );
+// }
+
+// function TodoItem({ todo }) {
+//   return <li>{todo}</li>;
+// }
+// const TodoApp = () => {
+//   return (
+//     <TodoProvider>
+//       <h1>Todo Application</h1>
+//       <TodoList />
+//       <TodoForm title={"Todo Form"} />
+//     </TodoProvider>
+//   );
+// };
+// export default TodoApp;
 import React, { useState, useContext, createContext } from "react";
+import TodoProvider, { TodoContext } from "./Context";
 
+// const TodoProvider = ({ children }) => {
+//   const [todo, setTodo] = useState("");
+//   const [todos, setTodos] = useState([]);
 
-const TodoContext = createContext();
+//   const addTodo = () => {
+//     setTodos([...todos, todo]);
+//     setTodo("");
+//   };
 
-function TodoApp() {
-  const [todo, setTodo] = useState("");
-  const [todos, setTodos] = useState([]);
+//   return (
+//     <TodoContext.Provider value={{ todos, setTodos, addTodo, todo, setTodo }}>
+//       {children}
+//     </TodoContext.Provider>
+//   );
+// };
 
-  const addTodo = () => {
-    setTodos([...todos, todo]);
-    setTodo("");
-  };
+const TodoForm = (props) => {
+  const { todo, setTodo, addTodo } = useContext(TodoContext);
 
   return (
-    <TodoContext.Provider value={{ todos, setTodos, addTodo, todo, setTodo }}>
-        <TodoList>Testing</TodoList>
-        <TodoForm title={"TodoForm"}
-        itemTodo={addTodo}
-        />
-    </TodoContext.Provider>
+    <>
+      {props.title}
+      <input
+        type="text"
+        value={todo}
+        placeholder="Add your todo"
+        onChange={(e) => setTodo(e.target.value)}
+      />
+      <button type="button" onClick={addTodo}>
+        Add Todo
+      </button>
+    </>
   );
-}
-const TodoForm = (props)=>{
-    const {todo, setTodo, addTodo} = useContext(TodoContext)
-    return (
-        <>
-        {props.title}
-     <input
-          type="text"
-          value={todo}
-          placeholder="Add your todo"
-          onChange={(e) => setTodo(e.target.value)}
-        />
-        <button type="submit" onClick={props.itemTodo}>
-          Add Todo
-        </button>
-        </>
-
-)}
+};
 
 function TodoList(props) {
-    console.log(props)
+  console.log(props);
   const { todos } = useContext(TodoContext);
 
   return (
     <ul>
       {todos.length > 0 &&
-        todos.map((todo, index) => (
-          <TodoItem key={index} todo={todo} />
-        ))}
+        todos.map((todo, index) => <TodoItem key={index} todo={todo} />)}
     </ul>
   );
 }
@@ -57,7 +113,14 @@ function TodoItem({ todo }) {
   return <li>{todo}</li>;
 }
 
+const TodoApp = () => {
+  return (
+    <TodoProvider>
+      <h1>Todo Application</h1>
+      <TodoList />
+      <TodoForm />
+    </TodoProvider>
+  );
+};
+
 export default TodoApp;
-
-
-
